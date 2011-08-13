5c05f39f7b5f2242e37a09b0b9102b76acad9ec8
Oliver Guenther <mail@oliverguenther.de>
2011-08-13 21:29:37 +0200

    First theme version

---
+++ lang/en/theme_tudarmstadt.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Strings for component 'theme_tudarmstadt', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   theme_tudarmstadt
+ * @copyright 2011 onwards Oliver Günther  {@link http://oliverguenther.de}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'TU-Darmstadt 2.0 Theme';
+$string['region-side-post'] = 'Right';
+$string['region-side-pre'] = 'Left';
+$string['choosereadme'] = 'Moodle Theme for Technische Universität Darmstadt';
